5e84414bc005e4a4cd2cf752ebe2a720083574de
Jorge2003216 <jorgemtzgeffroy@gmail.com>
2023-01-31 09:45:02 -0600

    Proyecto Hello World

---
+++ ionic-clase-2003216/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-clase-2003216',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
