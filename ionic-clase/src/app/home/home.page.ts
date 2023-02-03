d2d9e65f7614380e504bee8eb6df66710c0c95e7
Albertogg12 <120700072+Albertogg12@users.noreply.github.com>
2023-02-02 22:08:28 -0600

    actividad 1 - Alberto Garcia Garcia

---
+++ ionic-clase/src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core'; 
+ import { AlertController } from '@ionic/angular'; 
+  
+ @Component({ 
+   selector: 'app-home', 
+   templateUrl: 'home.page.html', 
+   styleUrls: ['home.page.scss'], 
+ }) 
+ export class HomePage { 
+  
+   constructor(private alertController: AlertController) {} 
+  
+   async presentAlert() { 
+     const alert = await this.alertController.create({ 
+       header: 'Mensaje de alerta', 
+       subHeader: 'Mensaje importante', 
+       message: '¡Este es un mensaje de alerta!', 
+       buttons: ['De Acuerdo'], 
+     }); 
+  
+     await alert.present(); 
+   } 
+  
+  
+ }
+ 
