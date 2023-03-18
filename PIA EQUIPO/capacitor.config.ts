5ba945be8f8d4fa8fdf0016f18b61210ac1ece82
Gsus523 <clase2324@gmail.com>
2023-03-18 08:20:40 -0600

    Primer avance PIA
    se entrega el primer avance del pia el cual es una pagina base que corre con el comando serve.

---
+++ PIA EQUIPO/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'casinopsis',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
