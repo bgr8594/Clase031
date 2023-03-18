f70bb69be91a3342ad223054130d28a812b81d8a
Joel Leon <joel.leonprz@uanl.edu.mx>
2023-03-17 22:22:03 -0600

    Initial Commit Equipo 3
    Se añade commit inicial para Equipo 3.
    * Se añade proyecto con Ionic, utilizando Angular
    * Se añade página en blanco de login

---
+++ pia-equipo3/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pia-equipo3',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
