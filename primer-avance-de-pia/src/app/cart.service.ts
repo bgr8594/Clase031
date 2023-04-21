6682abf4e0e1d32852826981a0a652881ed59863
Carlos Horta <daniel.hortamrn@uanl.edu.mx>
2023-04-21 17:14:46 -0600

    Carrito, parcial

---
+++ primer-avance-de-pia/src/app/cart.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  private cart = []
+
+  getCart(){
+    return this.cart;
+  }
+
+  /*
+  addProduct(product){
+    this.cart.push(product);
+  }
+  */
+
+
+  removeCart(){
+    this.cart = [];
+  }
+
+  constructor() { }
+}
--- primer-avance-de-pia/src/app/cart/cart.page.ts
+++ primer-avance-de-pia/src/app/cart/cart.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPage implements OnInit {
 
-  constructor() { }
+  selectedItems = [];
+  total = 0;
+
+  constructor(
+    private cartService: CartService,
+    private router: Router) {
+    }
 
   ngOnInit() {
+    let items = this.cartService.getCart();
+    let selected = {};
+
+    for(let obj of items){
+      /*selected[obj.id] = {...obj, count:1}; */
+    }
+
+    /* this.selectedItems = Object.keys(selected).map(key => selected[key]) */
+    console.log('items: ', this.selectedItems);
+    /*this.total = this.selectedItems.reduce((a,b) => a + (b.quantity *b.price), 0 );*/
   }
 
+  remove(){
+    this.cartService.removeCart();
+    this.router.navigate(["/menu"]);
+  }
+
+  checkout() {
+
+  }
 }
